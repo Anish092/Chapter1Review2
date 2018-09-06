5f732254605766f0492e7b97663bbf75eb5c00f7
Anish092 <37304018+Anish092@users.noreply.github.com>
2018-09-06 09:44:51 -0400

    first

--- imagetwo.java
+++ imagetwo.java
@@ -6,7 +6,7 @@ public class imagetwo
 	public static void main(String[] args) throws Exception
 	{
 		URL imageLocation = new URL(
-			"http://horstmann.com/java4everyone/duke.gif");
+			"https://codecondo.com/wp-content/uploads/2015/02/Java-Programmers.png");
 		JOptionPane.showMessageDialog(null, "What's Up", "Title",
 		      JOptionPane.PLAIN_MESSAGE, new ImageIcon(imageLocation));
     }
